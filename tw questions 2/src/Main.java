75173dff94591fc872275759bda6f5fe467416b9
avazowai <141987796+avazowai@users.noreply.github.com>
2023-08-14 16:33:30 +0600

    Add files via upload

---
+++ tw questions 2/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int [] a ={1,4,3,56,34,56};
+
+        for (int i = 0; i < a.length ; i++) {
+            if (a[i]>10){
+                System.out.println("Элемент a[" + i + "] = " + a[i] + " удовлетворяет условию a[i] > 10");
+            }
+        }
+
+
+
+
+
+
+    }
+}
